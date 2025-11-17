10317f1449961ef3b13eabda14f7878561096880
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-11-17 14:17:34 +0700

    feat: adding new cmd

---
+++ vendor/command-parser/main.php
@@ -0,0 +1,29 @@
+<?php
+
+use Cmd\Command;
+
+class CommandParser
+{
+    private array $args;
+    private array $commands;
+
+    public function __construct()
+    {
+        $this->args = $GLOBALS["argv"];
+    }
+
+    /**
+     * AddCmd adds new cmd handler to collection of commands
+     * @param Command $cmd
+     * @return void
+     * @throws InvalidArgumentException
+     * */
+    public function AddCmd(Cmd\Command $cmd): void
+    {
+        if (!$cmd instanceof Command) {
+            throw new InvalidArgumentException("unknown cmd type");
+        }
+
+        $this->commands[] = $cmd;
+    }
+}
