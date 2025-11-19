87dbd5d758b73463271712de6177072096ed4b6d
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-11-19 16:44:10 +0700

    feat: execute cmd handler

--- packages/command-parser/Commander.php
+++ packages/command-parser/Commander.php
@@ -1,7 +1,9 @@
 <?php
 namespace Packages\CommandParser;
 
+use Exception;
 use InvalidArgumentException;
+use RuntimeException;
 
 class Commander
 {
@@ -10,7 +12,7 @@ class Commander
     /** @var Command[] */
     private array $commands = [];
 
-    private DefaultCmd $defCommand;
+    private ?DefaultCmd $defCommand = null;
 
     public function __construct()
     {
@@ -18,17 +20,14 @@ class Commander
     }
 
     /**
-     * AddCmd adds new cmd handler to collection of commands
+     * AddCmd adds new cmd handler to collection of commands.
+     * if new cmd handler is the same with existing one, it'll calls the first cmd handler.
      * @param Command $cmd
      * @return void
      * @throws InvalidArgumentException
      * */
     public function AddCmd(Command $cmd): void
     {
-        if (!$cmd instanceof Command) {
-            throw new InvalidArgumentException("unknown cmd type");
-        }
-
         $this->commands[] = $cmd;
     }
 
@@ -49,10 +48,113 @@ class Commander
      */
     public function Execute(): void
     {
-        if (count($this->args) == 1) {
-            // Todo: make sure there's a default command
+        try {
+            $this->ExecuteE();
+        } catch (Exception $error) {
+            $this->DisplayMessage($error->getMessage());
+        }
+    }
+
+    /**
+     * Execute calls user-define function to run
+     * the application with throw an error.
+     * @return void
+     */
+    private function ExecuteE(): void
+    {
+        if (count($this->args) <= 1) {
+            $this->RunDefaultCmd();
+            return;
+        }
+
+        $commandRequest = $this->args[1] ?? "";
+
+        $isFound = $this->FindCmdHandler($commandRequest);
+
+        if ($isFound) {
+            $cmd = $this->GetCmdHandler($commandRequest);
+            $clearArgs = array_slice($this->args, 1);
+
+            $this->RunCmd($cmd, $clearArgs);
+        } else {
             $this->RunDefaultCmd();
         }
+    }
+
+    /**
+     * RunCmd run the associate command.
+     * @param Command $cmd the command handler
+     * @param array $args the arguments from command line
+     * @return void
+     * @throws \Exception
+     * @throws RuntimeException
+     */
+    private function RunCmd(Command $cmd, array $args): void
+    {
+        if (in_array("--help", $args)) {
+            $this->DisplayCommandInformation($cmd->description, $cmd->example);
+            return;
+        }
+
+        if (!is_callable($cmd->run)) {
+            throw new RuntimeException("command has no valid run() callback");
+        }
+
+        ($cmd->run)($args);
+    }
+
+    /**
+     * RunCmd run the associate command.
+     * @param Command $cmd the command handler
+     * @param array $args the arguments from command line
+     * @return void
+     * @throws \Exception
+     * @throws RuntimeException
+     */
+    private function DisplayCommandInformation(
+        string $description,
+        string $example,
+    ): void {
+        $msg = "{$description}\n\n";
+        $msg .= "Example: {$example}\n";
+
+        $this->DisplayMessage($msg);
+    }
+
+    /**
+     * FindCmdHandler finds the command object with the same command string passed.
+     * if found in the collection of commands it will return true for the first command
+     * @param string $command
+     * @return bool
+     */
+    private function FindCmdHandler(string $command): bool
+    {
+        $found = false;
+
+        foreach ($this->commands as $cmd) {
+            if ($cmd->use === $command) {
+                return $found = true;
+            }
+        }
+
+        return $found;
+    }
+
+    /**
+     * GetCmdHandler get the command object with the same command string passed.
+     * if found in the collection of commands it will return the first command
+     * @param string $command
+     * @return Command|null
+     */
+    private function GetCmdHandler(string $command): ?Command
+    {
+        foreach ($this->commands as $cmd) {
+            if ($cmd->use === $command) {
+                return $cmd;
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -65,6 +167,10 @@ class Commander
         echo "$msg \n";
     }
 
+    /**
+     * RunDefaultCmd run the defaultCmd that already register when there's no commands.
+     * @return void
+     */
     private function RunDefaultCmd(): void
     {
         $defaultCmd = $this->defCommand;
@@ -85,6 +191,8 @@ class Commander
             }
 
             $this->DisplayMessage($msg);
+        } else {
+            throw new Exception("default command not found");
         }
     }
 }
