568e5972d6359936c6ce428d3433f0f9500d3a53
faizisyellow <114222238+faizisyellow@users.noreply.github.com>
2025-11-20 21:09:13 +0700

    feat: integrate list command with storage

--- src/ListCommand.php
+++ src/ListCommand.php
@@ -1,19 +1,46 @@
 <?php
 namespace Faizisyellow\Songslists;
 
+use Exception;
+use Faizisyellow\Songslists\storage\storage;
 use Packages\CommandParser\Command;
 
 class ListCommand
 {
-    public static function build(): Command
+    private storage $store;
+
+    public function __construct(storage $store)
+    {
+        $this->store = $store;
+    }
+
+    public function build(): Command
     {
         $listCmd = new Command(
             "list",
             "list all songs",
             "php {file}.php list [args]",
         );
+
         $listCmd->run = function (array $args) {
-            echo "list called";
+            try {
+                $songs = $this->store->Get();
+
+                if (count($songs) === 0) {
+                    echo "empty songs";
+                    return;
+                }
+
+                $displaySongs = "";
+                foreach ($songs as $song) {
+                    $displaySongs .= "{$song["id"]}\t";
+                    $displaySongs .= "{$song["content"]}\n";
+                }
+
+                echo $displaySongs;
+            } catch (Exception $error) {
+                echo $error->getMessage();
+            }
         };
 
         return $listCmd;
